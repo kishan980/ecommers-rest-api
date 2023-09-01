61ce8d0b76491789d832e647cc4ecbe1fa798bb5
kishan980 <kishany980@gmail.com>
2023-09-01 19:24:15 +0530

    login and register and password are increpeted

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const userRouter = require("./router/user");
+const authRouter =require("./router/auth")
 
 dotenv.config({});
 mongoose
@@ -18,6 +19,7 @@ app.get("/hello", () => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth/", authRouter)
 app.listen(port, () => {
   console.log(`Backend server is running! ${port}`);
 });
--- router/auth.js
+++ router/auth.js
@@ -1,2 +1,58 @@
-const router =require("express").Router();
-module.exports=router
+const router = require("express").Router();
+const User = require("../model/user.model");
+const CryptoJS = require("crypto-js");
+const jwt =require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const saveUserNew = new User({
+    username,
+    email,
+    password: CryptoJS.AES.encrypt(
+      password,
+      process.env.SECRET_PASSWORD
+    ).toString(),
+  });
+  try {
+    const saveUser = await saveUserNew.save();
+    return res.send({
+      message: "User are register...",
+      status: 201,
+      data: saveUser,
+    });
+  } catch (error) {
+    return res.send({
+      message: `error is ${error}...`,
+      status: 500,
+      data: [],
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json({message:"Wrong Credentials!"})
+    const hashePassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET_PASSWORD
+    );
+
+    const passwordData =hashePassword.toString(CryptoJS.enc.Utf8);
+    passwordData !== req.body.password 
+    && res.status(401).json({message:"wrong credentials"})
+    const accessToken = jwt.sign({
+        id:user._id,
+        isAdmin:user.isAdmin
+    },process.env.JWT_SCERET,{expiresIn:process.env.JWT_EXPIRE})
+    const {password, ...others}=user._doc 
+    return res.status(200).json({
+        message:"user details",data:{...others,accessToken}
+    })
+  } catch (error) {
+    return res.status(500).json({message:`error ${error}`})
+
+  }
+});
+
+module.exports = router;
